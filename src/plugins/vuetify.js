8959984899053c395f18d6875df68b55fe3d729c
ruben-bustos <rubur100@gmail.com>
2021-08-18 20:41:31 +0200

    Create init vuetify theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,26 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            dark: {
+                primary: '#112D4E',
+                secondary: '#3F72AF',
+                accent: '#DBE2EF',
+                // error: '#F9F7F7',
+                info: '#2196F3',
+                success: '#4CAF50',
+                warning: '#FFC107',
+            },
+            light: {
+                primary: '#112D4E',
+                secondary: '#3F72AF',
+                accent: '#DBE2EF',
+                // error: '#F9F7F7',
+                info: '#2196F3',
+                success: '#4CAF50',
+                warning: '#FFC107',
+            }
+        }
+    }
 });
