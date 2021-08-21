ad41efbef8f45b89fd00eb24b6166b9dbe1e8a0a
ruben-bustos <rubur100@gmail.com>
2021-08-21 02:15:15 +0200

    Add avatar menu

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,58 @@
         <v-btn text outlined to="signup">Sign up</v-btn>
       </template>
       <template v-else>
-        <v-avatar color="purple" size="36">
-          <span class="white--text text-h5">{{ this.getUserCapitalLetter() }}</span>
-        </v-avatar>
+        <!-- <v-menu bottom
+      offset-y :nudge-width="200">
+          <template v-slot:activator="{ on, attrs }">
+            <v-avatar color="purple" size="36" v-bind="attrs" v-on="on">
+              <span class="white--text text-h5">{{
+                getUserCapitalLetter()
+              }}</span>
+            </v-avatar>
+          </template>
+
+          <v-list>
+            <v-list-item>
+              <v-list-item-title @click="logout()">Logout</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu> -->
+        <v-menu bottom min-width="200px" rounded offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn icon x-large v-on="on">
+              <v-avatar color="purple" size="38">
+                <span class="white--text text-h5">{{
+                  getUserCapitalLetter()
+                }}</span>
+              </v-avatar>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-list-item-content class="justify-center">
+              <div class="mx-auto text-center">
+                <v-avatar color="purple" size="48">
+                  <span class="white--text text-h5">{{
+                    getUserCapitalLetter()
+                  }}</span>
+                </v-avatar>
+                <h3>{{ user.full_name }}</h3>
+                <p class="text-caption mt-1">
+                  {{ user.email }}
+                </p>
+                <v-divider class="my-3"></v-divider>
+                <!-- <v-btn
+                depressed
+                rounded
+                text
+              >
+                Edit Account
+              </v-btn> -->
+                <!-- <v-divider class="my-3"></v-divider> -->
+                <v-btn depressed rounded text @click="logout()"> Sign out </v-btn>
+              </div>
+            </v-list-item-content>
+          </v-card>
+        </v-menu>
       </template>
     </v-app-bar>
 
@@ -30,7 +79,7 @@
 
 <script>
 import authService from "@/services/authService.js";
-import Upload from './views/Upload.vue'
+import Upload from "./views/Upload.vue";
 
 export default {
   name: "App",
@@ -39,15 +88,26 @@ export default {
   },
   data: () => ({
     showUploadForm: false,
+    user: null,
   }),
   methods: {
     isUserLogged() {
       return authService.isUserLogged();
     },
-    getUserCapitalLetter() {
-      // TODO: improve logic and syntax sugar
-      return authService.getUserLogged().full_name.split("")[0].toUpperCase();
+    logout() {
+      authService.logout();
+      this.$router.push("/login");
     },
-  }
+    getUserLogged() {
+      this.user = authService.getUserLogged();
+    },
+    getUserCapitalLetter() {
+      return this.user.full_name.split("")[0].toUpperCase();
+    },
+  },
+  created() {
+    document.title = "Jellytoring";
+    this.getUserLogged();
+  },
 };
 </script>
